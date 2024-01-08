523fc2f03bdc15a5b77d0fb558435258c62b931e
Pedro <pedrodamiao26061998@gmail.com>
2024-01-08 17:18:47 +0000

    atualizado

--- Portfolio_Pedro/script.js
+++ Portfolio_Pedro/script.js
@@ -1,7 +1,17 @@
 
 let butmenu = document.getElementById('but-abrir-menu-mob')
 let menumobile = document.getElementById('menu-mobile')
+let overlay = document.getElementById('overlay-menu-mob')
+
 
 butmenu.addEventListener('click', ()=>{
     menumobile.classList.add('abrir_menu')
+})
+
+menumobile.addEventListener('click', ()=>{
+    menumobile.classList.remove('abrir_menu')
+})
+
+overlay.addEventListener('click', ()=>{
+    menumobile.classList.remove('abrir_menu')
 })
