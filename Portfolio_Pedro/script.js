036f7857a73dcf8661c80922d54ec445a949339f
Pedro <pedrodamiao26061998@gmail.com>
2024-01-08 12:04:06 +0000

    Atualizado

---
+++ Portfolio_Pedro/script.js
@@ -0,0 +1,7 @@
+
+let butmenu = document.getElementById('but-abrir-menu-mob')
+let menumobile = document.getElementById('menu-mobile')
+
+butmenu.addEventListener('click', ()=>{
+    menumobile.classList.add('abrir_menu')
+})
