8a958f06cabd648163ad5075f0d1ea7c1e17be79
Pedro <pedrodamiao26061998@gmail.com>
2024-01-09 11:55:19 +0000

    Atualizado

---
+++ Portfolio_Pedro/JavaScript/reveal.js
@@ -0,0 +1,12 @@
+
+window.revelar = ScrollReveal({reset:true})
+
+revelar.reveal('.efeito_text_topo_site',{
+    duration: 2000,
+    distance: '90px'
+})
+
+revelar.reveal('.efeito_img_topo_site',{
+    duration: 2000,
+    distance: '90px'
+})
