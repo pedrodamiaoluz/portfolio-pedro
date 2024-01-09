bf39bdd72cc7137fa0ef3bcc658b3e48078462d2
Pedro <pedrodamiao26061998@gmail.com>
2024-01-09 17:26:38 +0000

    Atualizado

--- Portfolio_Pedro/JavaScript/reveal.js
+++ Portfolio_Pedro/JavaScript/reveal.js
@@ -1,4 +1,4 @@
-
+//Topo do site
 window.revelar = ScrollReveal({reset:true})
 
 revelar.reveal('.efeito_text_topo_site',{
@@ -8,5 +8,123 @@ revelar.reveal('.efeito_text_topo_site',{
 
 revelar.reveal('.efeito_img_topo_site',{
     duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+
+// Os titulos das seções
+revelar.reveal('.titulo',{
+    duration: 2000,
     distance: '90px'
 })
+
+// Texto sobre mim
+
+revelar.reveal('.text_sobre_mim',{
+    duration: 2000,
+    distance: '90px'
+})
+
+// Seções dos button das redes sociais sobre mim
+
+revelar.reveal('.but_rede_sociais1',{
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+
+revelar.reveal('.but_rede_sociais2',{
+    duration: 2000,
+    distance: '90px',
+    delay: 1000
+})
+
+revelar.reveal('.but_rede_sociais3',{
+    duration: 2000,
+    distance: '90px',
+    delay: 1500
+})
+
+revelar.reveal('.but_rede_sociais4',{
+    duration: 2000,
+    distance: '90px',
+    delay: 2000
+})
+
+// Seções das minhas Habilidades
+
+revelar.reveal('.habilidade_box1',{
+    duration: 2000,
+    distance: '90px',
+    delay: 1000,
+    origin: 'left'
+})
+
+revelar.reveal('.habilidade_box2',{
+    duration: 2000,
+    distance: '90px',
+    delay: 1500,
+    origin: 'left'
+})
+
+revelar.reveal('.habilidade_box3',{
+    duration: 2000,
+    distance: '90px',
+    delay: 2000,
+    origin: 'left'
+})
+
+// Seções do Meu Portfólio
+
+revelar.reveal('.img_port-1',{
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+
+revelar.reveal('.img_port-2',{
+    duration: 2000,
+    distance: '90px',
+    delay: 1000
+})
+
+revelar.reveal('.img_port-3',{
+    duration: 2000,
+    distance: '90px',
+    delay: 1500
+})
+
+// Seção do Formulário
+
+revelar.reveal('.form-1',{
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+
+revelar.reveal('.form-2',{
+    duration: 2000,
+    distance: '90px',
+    delay: 1000
+})
+
+revelar.reveal('.form-3',{
+    duration: 2000,
+    distance: '90px',
+    delay: 1500
+})
+
+revelar.reveal('.textarea',{
+    duration: 2000,
+    distance: '90px',
+    delay: 2000
+})
+
+revelar.reveal('.button_contate_me',{
+    duration: 2000,
+    distance: '90px',
+    delay: 2500
+})
+
+
+
