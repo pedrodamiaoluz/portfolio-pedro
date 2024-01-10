16ef057bf6d03bf1c2f31f00068422d95f8a87c6
Pedro <pedrodamiao26061998@gmail.com>
2024-01-10 11:46:14 +0000

    Atualizado

--- Portfolio_Pedro/Js/script.js
+++ Portfolio_Pedro/Js/script.js
@@ -1,4 +1,4 @@
-
+// Menu Mobile
 let butmenu = document.getElementById('but-abrir-menu-mob')
 let menumobile = document.getElementById('menu-mobile')
 let overlay = document.getElementById('overlay-menu-mob')
@@ -15,8 +15,30 @@ menumobile.addEventListener('click', ()=>{
 overlay.addEventListener('click', ()=>{
     menumobile.classList.remove('abrir_menu')
 })
+// fim do Menu Mobile
 
+// Do Header
 window.addEventListener("scroll", function(){
     let header = document.querySelector('#header')
     header.classList.toggle('rolagem', window.scrollY > 0)
 })
+// Fim do Header
+
+// Formulário
+    let form = document.querySelector('#form')
+    let nome = document.querySelector('input#formnome')
+    let email = document.querySelector('input#email')
+    let telefone = document.querySelector('input#telefone')
+    let assunto = document.querySelector('textarea#assunto')
+    
+
+    window.addEventListener('submit', (enviar) => {
+        enviar.preventDefault()
+
+        if(nome.value == ''){
+            alert('erro')
+        }
+
+        form.submit()
+    })
+
