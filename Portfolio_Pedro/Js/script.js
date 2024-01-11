a6a4bdf0370dc55fb2ee91c8f47957584dada04d
Pedro <pedrodamiao26061998@gmail.com>
2024-01-11 11:42:03 +0000

    Atualizado

--- Portfolio_Pedro/Js/script.js
+++ Portfolio_Pedro/Js/script.js
@@ -18,11 +18,12 @@ overlay.addEventListener('click', ()=>{
 // fim do Menu Mobile
 
 // Do Header
-window.addEventListener("scroll", function(){
+/*window.addEventListener("scroll", function(){
     let header = document.querySelector('#header')
     header.classList.toggle('rolagem', window.scrollY > 0)
 })
 // Fim do Header
+*/
 
 // Formulário
     let form = document.querySelector('#form')
@@ -37,16 +38,14 @@ window.addEventListener("scroll", function(){
         //verificar se o nome esta vazio
         if(nome.value == ''){
             alert('Por favor, preencha seu nome!!!')
-            return
+            return  
         }
+        //Verificando se o email esta fazio e esta validando
         else if(email.value == '' || !validaremail(email.value)){
             alert('Por favor, preencha seu email!!!')
             return 
         }
-        else if(telefone.value == '' || !telefones(telefone.value)){
-            alert('Número de telefone invalido!!!')
-            return 
-        }
+        //Verificando se o assunto esta fazio
         else if(assunto.value == ''){
             alert('Por favor, Informe a Mensagem!!!')
             return 
