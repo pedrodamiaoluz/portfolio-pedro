a156c67a699229f72534571fe2447dbf305be241
Pedro <pedrodamiao26061998@gmail.com>
2024-01-13 10:19:39 +0000

    Atualizado

--- Js/scrollReveal.js
+++ Js/scrollReveal.js
@@ -97,31 +97,31 @@ revelar.reveal('.img_port-3',{
 // Seção do Formulário
 
 revelar.reveal('.form-1',{
-    duration: 900,
+    duration: 800,
     distance: '90px',
     delay: 500
 })
 
 revelar.reveal('.form-2',{
-    duration: 900,
+    duration: 800,
     distance: '90px',
     delay: 1000
 })
 
 revelar.reveal('.form-3',{
-    duration: 900,
+    duration: 800,
     distance: '90px',
     delay: 1500
 })
 
 revelar.reveal('.textarea',{
-    duration: 900,
+    duration: 800,
     distance: '90px',
     delay: 2000
 })
 
 revelar.reveal('.button_contate_me',{
-    duration: 900,
+    duration: 800,
     distance: '90px',
     delay: 2500
 })
